8544957049d3725075ff932500edc7df1c9006b2
b2pwr <e54096409@gmail.com>
2020-09-11 12:54:19 +0800

    feat: update InfiniteScroll

--- src/front-end/components/InfiniteScroll/index.js
+++ src/front-end/components/InfiniteScroll/index.js
@@ -1,72 +1,78 @@
 import React from 'react';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Avatar from '@material-ui/core/Avatar';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  spinner: {
+    display: 'flex',
+    justifyContent: 'center',
+    height: 60,
+  },
+});
 
 class InfiniteScroll extends React.Component {
   constructor() {
     super();
+    this.state = {
+      isBottom: false,
+    };
     this.observeRef = React.createRef();
   }
 
-  componentDidUpdate = () => {
+  componentDidMount = () => {
     const node = this.observeRef.current;
-    if(node) {
-      var options = {
-        root: null, // viewport
-        rootMargin: '0px 0px 300px 0px',
-        threshold: 1.0,
-      };
-      
-      this.observer = new IntersectionObserver(
-        this.callback,
-        options
-      );
-      this.observer.observe(node);
-    }
+    var options = {
+      root: null, // viewport
+      rootMargin: '0px 0px 300px 0px',
+      threshold: 1.0,
+    };
+    
+    this.observer = new IntersectionObserver(
+      this.callback,
+      options
+    );
+    this.observer.observe(node);
   }
 
   callback = (entries, observer) => {
     const {
       loadMore,
+      children,
     } = this.props;
     entries.forEach(entry => {
-      if(entry.isIntersecting) {
+      if(entry.isIntersecting && children.props.children.length > 0) {
         loadMore();
+        this.setState({
+          isBottom: true,
+        })
+      } else {
+        this.setState({
+          isBottom: false,
+        })
       }
     })
   }
 
   render() {
     const {
-      repos,
+      classes,
+      children,
+      spinner,
     } = this.props;
+    const {
+      isBottom,
+    } = this.state;
     return (
       <div>
         <div>
-        <List component="nav">
-          {repos.map(repo => (
-            <ListItem
-              button
-              key={repo.id}
-              onClick={() => {
-                window.open(repo.html_url, '_blank', 'noopener,resizable,scrollbars');
-              }
-            }>
-              <ListItemIcon>
-                <Avatar alt={repo.name} src={repo.owner.avatar_url} />
-              </ListItemIcon>
-              <ListItemText primary={repo.full_name} secondary={repo.description} />
-            </ListItem>
-          ))}
-        </List>
+          {children}
         </div>
-        {repos.length > 0 && <div ref={this.observeRef} className="observe"></div>}
+        <div className={classes.spinner}>
+          {isBottom && children.props.children.length > 0 && spinner }
+        </div>
+        <div ref={this.observeRef} className="observe"></div>
       </div>
     );
   }
 }
 
-export default InfiniteScroll;
+export default withStyles(styles)(InfiniteScroll);
--- src/front-end/containers/App/index.js
+++ src/front-end/containers/App/index.js
@@ -5,6 +5,12 @@ import { debounce } from 'lodash';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import LinearProgress from '@material-ui/core/LinearProgress';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
 import * as Selectors from './selectors';
 import AppBar from '../../components/AppBar';
 import InfiniteScroll from '../../components/InfiniteScroll';
@@ -38,14 +44,30 @@ class App extends React.Component {
         <AppBar inputChange={debounceInput} />
         {isSearching &&  <LinearProgress />}
         <InfiniteScroll
-          repos={repos}
           loadMore={() => {
             if(!isSearching) {
               loadMore(keywords, nextPage);
             }
           }}
-          nextPage={nextPage}
-        />
+          spinner={<CircularProgress />}
+        >
+          <List component="nav">
+            {repos.map(repo => (
+              <ListItem
+                button
+                key={repo.id}
+                onClick={() => {
+                  window.open(repo.html_url, '_blank', 'noopener,resizable,scrollbars');
+                }
+              }>
+                <ListItemIcon>
+                  <Avatar alt={repo.name} src={repo.owner.avatar_url} />
+                </ListItemIcon>
+                <ListItemText primary={repo.full_name} secondary={repo.description} />
+              </ListItem>
+            ))}
+          </List>
+        </InfiniteScroll>
       </ThemeProvider>
     );
   }
